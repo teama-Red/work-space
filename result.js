b0fab3158dd3b3f26475f850eae4446820d363e7
chiyoho <chiyoho@chiyohonoMacBook-Pro.local>
2024-01-30 09:39:41 +0900

    revealResult関数の追加、cssの軽微な変更

--- result.js
+++ result.js
@@ -3,6 +3,17 @@
 // HTMLのonclickで呼び出す関数
 function seeResult(){
     // テスト用
-    alert("hello");
+    // alert("hello");
     document.getElementById('result').innerHTML = '<h1>Test<h1>'
+    revealResult();
+}
+
+// 表示非表示の切り替えを行う関数
+function revealResult() {
+    // テスト用
+    alert("hello");
+    document.getElementById('home').classList.remove('d-block');
+    document.getElementById('home').classList.add('d-none');
+    document.getElementById('result').classList.remove('d-none');
+    document.getElementById('result').classList.add('d-block');
 }
